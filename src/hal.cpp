08dd5bbf8c1cc84dfa77428dce0f64856427baa3
Brendan Burns <brendan.d.burns@gmail.com>
2024-09-26 21:51:56 +0000

    Move to new lib version.

--- src/hal.cpp
+++ src/hal.cpp
@@ -106,63 +106,75 @@ uint8_t HardwareAbstractionLayer::GetModType(uint8_t addr) {
 }
 
 bool HardwareAbstractionLayer::DefineStatus(uint8_t addr, int flags) {
-    return ((ServoModule*)bus->getModule(addr))->defineStatus(flags);
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->defineStatus(flags);
 }
 
 void HardwareAbstractionLayer::NoOp(uint8_t addr) {
-    ((ServoModule*)bus->getModule(addr))->noop();
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    static_cast<ServoModule*>(module.get())->noop();
 }
 
 void HardwareAbstractionLayer::InitPath(uint8_t addr) {
-    ((ServoModule*)bus->getModule(addr))->initPath();
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    static_cast<ServoModule*>(module.get())->initPath();
 }
 
 bool HardwareAbstractionLayer::StartPathMode(uint8_t group_addr, uint8_t leader_addr) {
-    return ((ServoModule*)bus->getModule(leader_addr))->startPathMode(group_addr);
+    std::shared_ptr<Module> module = bus->getModule(leader_addr);
+    return static_cast<ServoModule*>(module.get())->startPathMode(group_addr);
 }
 
 bool HardwareAbstractionLayer::AddPathpoints(uint8_t addr, size_t num, long *pts) {
-    auto points = new int16_t[num];
+    auto points = std::vector<int16_t>(num);
     for (size_t i = 0; i < num; i++) {
         points[i] = pts[i];
     }
-    auto result = ((ServoModule*)bus->getModule(addr))->addPathPoints(num, points);
-    delete [] points;
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    auto result = static_cast<ServoModule*>(module.get())->addPathPoints(points);
     return result;
 }
 
 bool HardwareAbstractionLayer::ResetPos(uint8_t addr) {
-    return ((ServoModule*)bus->getModule(addr))->zero();
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->zero();
 }
 
 bool HardwareAbstractionLayer::LoadTraj(uint8_t addr, uint8_t flags, int32_t position, uint32_t velocity, uint32_t acceleration, uint8_t pwm) {
-    return ((ServoModule*)bus->getModule(addr))->loadTrajectory(flags, position, velocity, acceleration, pwm);
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->loadTrajectory(flags, position, velocity, acceleration, pwm);
 }
 
 bool HardwareAbstractionLayer::StopMotor(uint8_t addr, uint8_t flags) {
-    return ((ServoModule*)bus->getModule(addr))->stop(flags);
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->stop(flags);
 }
 
 long HardwareAbstractionLayer::GetPos(uint8_t addr) {
-    return ((ServoModule*)bus->getModule(addr))->getPos();
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->getPos();
 }
 
 void HardwareAbstractionLayer::GetPosAndVel(uint8_t addr, int* pos, int* vel) {
-    *pos = ((ServoModule*)bus->getModule(addr))->getPos();
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    *pos = static_cast<ServoModule*>(module.get())->getPos();
     // Velocity is in ticks / servo cycle, and the servo runs at 1953 Hz, translate to clicks/sec.
-    *vel = 1953 * ((ServoModule*)bus->getModule(addr))->getVel();
+    *vel = 1953 * static_cast<ServoModule*>(module.get())->getVel();
 }
 
 bool HardwareAbstractionLayer::SetGain(uint8_t addr, long Kp, long Kd, long Ki, long IL, long OL, long CL, long EL, long SR, long DC) {
-    return ((ServoModule*)bus->getModule(addr))->setGain(Kp, Kd, Ki, IL, OL, CL, EL, SR, DC);
+    std::shared_ptr<Module> module = bus->getModule(addr);
+    return static_cast<ServoModule*>(module.get())->setGain(Kp, Kd, Ki, IL, OL, CL, EL, SR, DC);
 }
 
 bool HardwareAbstractionLayer::Moving(uint8_t addr) {
     uint8_t statbyte = 0;
-    if (!((ServoModule*)bus->getModule(addr))->noop()) {
+    std::shared_ptr<Module> module = bus->getModule(addr);
+
+    if (!static_cast<ServoModule*>(module.get())->noop()) {
         return false;
     }
-    statbyte = ((ServoModule*)bus->getModule(addr))->getStat();
+    statbyte = static_cast<ServoModule*>(module.get())->getStat();
     return !(statbyte & MOVE_DONE);
 }
 #endif // OLDNMC
