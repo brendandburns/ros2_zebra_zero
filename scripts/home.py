220eba7fb787800e5f62afaad685722a8edc15a2
Brendan Burns <brendan.d.burns@gmail.com>
2024-10-09 15:20:03 +0000

    Update scripts

--- scripts/home.py
+++ scripts/home.py
@@ -13,12 +13,12 @@ def main():
     joint_trajectory.joint_names = ['first_joint', 'second_joint', 'third_joint', 'fourth_joint', 'fifth_joint', 'sixth_joint']
 
     # Add waypoints (JointTrajectoryPoints) to the trajectory
-    for i in range(0, 15):
-        point = JointTrajectoryPoint()
-        point.positions = [0.0, 0.1 * i, -0.1 * i, 0.0, -0.1 * i, 0.0]  # Joint positions at time t=0
-        # point.positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        point.time_from_start.sec = i 
-        joint_trajectory.points.append(point)
+    #for i in range(0, 15):
+    #    point = JointTrajectoryPoint()
+    #    point.positions = [0.0, 0.1 * i, -0.1 * i, 0.0, -0.1 * i, 0.0]  # Joint positions at time t=0
+    #    # point.positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    #    point.time_from_start.sec = i 
+    #    joint_trajectory.points.append(point)
     point = JointTrajectoryPoint()
     point.positions = [0.0, 1.57, -1.57, 0.0, -1.57, 0.0]
     point.time_from_start.sec = 15
--- scripts/switch_to_zerog.sh
+++ scripts/switch_to_zerog.sh
@@ -5,5 +5,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 $SCRIPT_DIR/deactivate_controllers.sh
-
-ros2 control switch_controllers --activate cartesian_motion_controller motion_control_handle
+ros2 control switch_controllers --activate zerog_controller
